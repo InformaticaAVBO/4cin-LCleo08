b9205881aac6b2ef9320b918742127812f7aec94
leonardo cazzanti <leonardocazzanti@gmail.com>
2025-11-12 09:24:45 +0000

    serie a

--- Copyfile/src/App.java
+++ Copyfile/src/App.java
@@ -1,31 +1,47 @@
 import java.io.File;
-import java.io.FileWriter;   // Import the FileWriter class
-import java.io.IOException;  // Import the IOException class
-import java.util.Scanner;    // Import the Scanner class to read text files
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        String file;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Inserire il nome del file da creare:");
-        file=input.nextLine();
-        FileWriter scrivi0 = new FileWriter(file);
-        System.out.println("Inserire il nome del secondo file da creare:");
-        file=input.nextLine();
-        FileWriter scrivi1 = new FileWriter(file);
 
-        FileWriter myWriter = new FileWriter("filename.txt");
-        for(int i=0; i<10; i++) myWriter.write("Testo numero "+i+"\n");
-        myWriter.close();  // must close manually
-        System.out.println("Fatto");
-
-        File fail = new File("filename.txt");
-        Scanner leggi = new Scanner(file);
-        while(leggi.hasNextLine()) {
+    public static void copyFile( String fileIn, String fileOut ) throws IOException {
+        File f = new File(fileIn);
+        Scanner leggi = new Scanner(f);
+        FileWriter scrivi = new FileWriter(fileOut);
+        // legge riga per riga e le scrive in output
+        while (leggi.hasNextLine()) {
             String s = leggi.nextLine();
-            System.out.println(s);
+            scrivi.write(s);
         }
-        leggi.close();  // must close manually
-        
+        leggi.close();
+        scrivi.close();
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        // chiede i nomi dei file all'utente
+        Scanner leggiDaTastiera = new Scanner(System.in);
+        System.out.print("Nome del file di input: ");
+        String inputFilename = leggiDaTastiera.nextLine();
+        System.out.print("Nome del file di output: ");
+        String outputFilename = leggiDaTastiera.nextLine();
+        leggiDaTastiera.close();
+
+        // copia input in output
+        copyFile( inputFilename, outputFilename);
+
+
+
+        /*
+        FileWriter myWriter = new FileWriter("filename.txt");
+        for (int i=0; i<100; i++) {
+            myWriter.write("Questa è una prima frase scritta in un file\n");
+        }
+        myWriter.close();
+        System.out.println("Ok, ho scritto sul file!");
+        */
+
     }
 }
